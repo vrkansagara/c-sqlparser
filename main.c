7146025056bbe8bfbebadae920c1daaeccbd60c8
x0rld <thomas.sam78@gmail.Com>
2020-02-09 23:31:23 +0100

    add dynamic create file and make error log

--- main.c
+++ main.c
@@ -8,32 +8,50 @@ int readFile(FILE* fpsrc, FILE* fpdest);
 
 int main() {
     FILE *fp = NULL;
+    FILE *newfp = NULL;
     FILE *crashfp = NULL;
     DIR *dir;
     struct  dirent* ent;
     int count = 0;
+    char  path[256];
 
     dir = opendir("../sql/");
     if (!dir)
     {
-        crashfp =fopen("../crash.log","a");
-        fprintf(crashfp, "fichier x introuvable\n");
-        closedir(dir);
+        crashfp = fopen("../crash.log","a");
+        fprintf(crashfp, "dossier sql introuvable\n");
+        fclose(crashfp);
         return EXIT_FAILURE;
     }
-
+    newfp = fopen("../new.sql","a");
+    if (!newfp)
+    {
+        crashfp = fopen("../crash.log","a");
+        fprintf(crashfp, "fichier de fusion impossible à creer\n");
+        fclose(crashfp);
+        return EXIT_FAILURE;
+    }
     while ((ent = readdir (dir))!= NULL)
     {
         if (strstr(ent->d_name,".sql")) {
+            strcpy(path,"../sql/");
             count++;
+            strcat(path,ent->d_name);
+            puts(path);
+            fp = fopen(path,"r");
+            if (!fp)
+            {
+                crashfp = fopen("../crash.log","a");
+                fprintf(crashfp, "fichier introuvable %s \n",ent->d_name);
+                fclose(crashfp);
+                return EXIT_FAILURE;
+            }
+            printf("lignes copiees %d\n",readFile(fp,newfp));
         }
 
     }
-    printf("%d",count);
+    printf("nombre de fichiers sql trouves:%d",count);
     closedir(dir);
-    for (int i = 0; i <count ; ++i) {
-
-    }
 
     return EXIT_SUCCESS;
 }
